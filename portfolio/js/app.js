25a2f844928b0e18d7addce2cbc0089844700406
MOONcitizenX <mooncitizenx@gmail.com>
2022-05-16 22:49:22 +0300

    feat: add burger menu and burger menu animation at <= 768px

---
+++ portfolio/js/app.js
@@ -0,0 +1,58 @@
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.header__list');
+const burgerOpen = document.querySelector('.burger__icon-open');
+const burgerClose = document.querySelector('.burger__icon-close');
+const navMenuItems = document.querySelectorAll('.header__list-link');
+const darkBg = document.querySelector('#cover');
+
+const bgClick = () => {
+	darkBg.addEventListener('click', () => {
+		navMenu.classList.remove('burger__active');
+		toggleBurger();
+		darkenBg();
+	});
+};
+
+const burgerButton = () => {
+	burger.addEventListener('click', () => {
+		navMenu.classList.toggle('burger__active');
+		toggleBurger();
+		darkenBg();
+	});
+};
+
+const navLinks = () => {
+	navMenuItems.forEach((link) => {
+		link.addEventListener('click', () => {
+			navMenu.classList.remove('burger__active');
+			toggleBurger();
+			darkenBg();
+		});
+	});
+};
+
+const darkenBg = () => {
+	if (darkBg.classList.contains('covering')) {
+		darkBg.classList.remove('covering');
+	} else {
+		darkBg.classList.add('covering');
+	}
+};
+
+const toggleBurger = () => {
+	if (navMenu.classList.contains('burger__active')) {
+		burgerOpen.classList.add('burger__close');
+		burgerClose.classList.remove('burger__close');
+	} else {
+		burgerClose.classList.add('burger__close');
+		burgerOpen.classList.remove('burger__close');
+	}
+};
+
+const app = () => {
+	burgerButton();
+	navLinks();
+	bgClick();
+};
+
+app();
